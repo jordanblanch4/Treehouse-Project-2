c46f5c93a2b1abc7a033d88cab51e724abf5b559
Jordan Blanchard <jordanb17b@icloud.com>
2020-04-06 23:28:24 -0700

    update scriptjs

--- js/script.js
+++ js/script.js
@@ -1,100 +1,63 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-let listItem = document.querySelectorAll('.student-item');
+let listItem = document.getElementsByClassName('student-item');
 let showItems = 10;
+const pageDaddy = document.querySelector('.page');
 
-
-
-
-//list[i]>=startIndex && list[i]<endIndex
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 
 function showPage (list, page) {
    const startIndex = (page * showItems)-showItems;
-   const endIndex = page * showItems;
+   const endIndex = (page * showItems) - 1;
    for (let i=0; i<list.length; i++) {
-      if(i >= startIndex && i < endIndex) {
-         list[i].style.display = 'block';
+      if(i >= startIndex && i <= endIndex) {
+         list[i].style.display = ' ';
       } else {
          list[i].style.display = 'none';
    }
-   }
-}
+   };
+};
 
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
 function appendLinks(list) {
-   const pageDaddy = document.getElementsByClassName('page');
+  
+   const amount = Math.ceil(list.length/showItems);
    let div = document.createElement('div');
-   div.className = 'pagination';
    pageDaddy.appendChild(div);
+   div.className = 'pagination';
    
    const ul = document.createElement('ul');
    div.appendChild(ul);
-   const li = document.createElement(li);
-
-   const amount = list.length/10;
-   for(let i =0; i<=amount; i++) {
-      li.innterHTML ='<a href="#"';
-      li.textConent = i+1;
-      if(i === 0) {
-         li.className = 'active';
-      }
-      ul.appendChild('li');
-   }
-   const anchor = document.getElemenetBy
-   a.addEventListener('click', (e)=> {
-      const pageLinks = document.querySelectorAll(".pagination a")
    
-      for(let i=0; i<pageLinks.length; i++) {
-         pageLinks[i].className = '';
+   for (let i =0; i < amount; i++) {
+      const anch = document.createElement('a');
+      const li = document.createElement('li');
+      
+      li.appendChild(anch);
+      ul.appendChild(li);
+      
+      let pageLinks = document.getElementsByTagName('a');
+      anch.setAttribute = ("href", "#");
+      
+      if(i === 0) {
+         anch.className = 'active';
       }
-   let activeClass = event.target;
-   activeClass.className = 'active';
-   showPage(list, event.target.textContent);
+   
+   anch.textContent = i+1;
+   
+   div.addEventListener('click', (event) => {
+      
+      showPage(listItem, parseInt(event.target.textContent));
+
+      for(j=0; j<pageLinks.length; j++) {
+         pageLinks[j].className = '';
+      }
+   
+   event.target.className = 'active';
    
    });
-}
-
-
+};
+};
 
 showPage(listItem, 1);
 appendLinks(listItem);
 
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
